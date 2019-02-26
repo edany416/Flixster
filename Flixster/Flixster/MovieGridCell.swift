5d4b94a41a4588ac14ec09ec50e224e2885e7f78
edanyachdav <edan.yachdav@gmail.com>
2019-02-25 16:05:46 -0800

    Flixster Unit 2

---
+++ Flixster/Flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by edan yachdav on 2/25/19.
+//  Copyright © 2019 edan yachdav. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
